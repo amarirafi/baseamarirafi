8e7f6bfd172f4008925ac1dbe3edbffb54497c1c
Rafi Amari <39509465+amarirafi@users.noreply.github.com>
2023-07-23 09:50:33 -0700

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xa7c0b58c99ecfbfe7b157cd58eae28819e82ba52";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
